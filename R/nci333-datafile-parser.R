9223aaa148ed301ac64e386260e05c4941bd8c1a
Tyler Schenk <schenk.tyler@gmail.com>
2015-06-30 11:09:29 -0500

    csv to chna

--- R/nci333-datafile-parser.R
+++ R/nci333-datafile-parser.R
@@ -2,7 +2,6 @@
 spss_to_pilr_chna <- function(data, params, ...) {
   # Read in SPSS file
   b64_decoded_raw <- rawToChar(base64decode(params$files$spss_chna_file))
-  stop(paste0("test"))
   df <- read.spss(textConnection(b64_decoded_raw), to.data.frame = TRUE)
   
   # Rename and select vars to return
@@ -23,6 +22,29 @@ spss_to_pilr_chna <- function(data, params, ...) {
   ret
 }
 
+#' @export
+csv_to_pilr_chna <- function(data, params, ...) {
+  # Read in SPSS file
+  b64_decoded_raw <- rawToChar(base64decode(params$files$csv_chna_file))
+  df <- read.csv(textConnection(b64_decoded_raw), to.data.frame = TRUE)
+  
+  # Rename and select vars to return
+  df <- select(df, c(caseid_1 = CaseID_1, city_rec = city_recode,
+                     sc_6 = SC_6, sc_127 = SC_127, sc_129 = SC_129,
+                     sc_130 = SC_130, sc_156 = SC_156, sc_176 = SC_176,
+                     sc_177 = SC_177, sc_178 = SC_178, sc_184 = SC_184,
+                     sc_185 = SC_185, sc_186 = SC_186, bmi_stat = bmi_status,
+                     weight = Weight, age_reco = age_recode, sc15reco = SC15recode,
+                     sc16reco = SC16recode, mpamin = MPAmin, vpamin = VPAmin,
+                     mvpamin = MVPAmin, mpa = MPA, vpa = VPA, mvpa = MVPA,
+                     `filter_$` = filter_.))
+  
+  # Construct return list
+  datasets <- list(csv_chna = df)
+  files <- list()
+  ret <- list(datasets = datasets, files = files)
+  ret
+}
 
 #' @export
 sas_to_pilr_chr <- function(data, params, ...) {
--- tests/testing.R
+++ tests/testing.R
@@ -3,8 +3,18 @@ library(sas7bdat)
 
 test <- read.spss("C:/Users/Tyler/Documents/NCI333/SPSS/SPSS/Story County_online survey.sav",
                   to.data.frame = TRUE)
-test2 <- read.spss("C:/Users/Tyler/Documents/NCI333/SPSS/SPSS/Story_County_health_needs_assessment2010_weighted.sav",
+test2 <- read.spss("C:/Users/Tyler/Documents/NCI333/Data/Story_County_health_needs_assessment2010_weighted.sav",
                   to.data.frame = TRUE)
 
 test3 <- read.sas7bdat("C:/Users/Tyler/Documents/NCI333/Data/nationaldataset2014.sas7bdat")
 test4 <- read.csv("C:/Users/Tyler/Documents/NCI333/Data/2014 CHR analytic data.csv")
+b64 <- base64encode("C:/Users/Tyler/Documents/NCI333/Data/2014 CHR analytic data.csv")
+b64_decoded_raw <- rawToChar(base64decode(b64))
+df <- read.csv(textConnection(b64_decoded_raw))
+
+## Convert SPSS to CSV
+data <- read.spss("C:/Users/Tyler/Documents/NCI333/Data/Story_County_health_needs_assessment2010_weighted.sav",
+                  to.data.frame = TRUE)
+write.csv(data, "test.csv")
+
+data2 <- read.csv("C:/Users/Tyler/Documents/NCI333/Data/Story_County_health_needs_assessment2010_weighted.csv")
